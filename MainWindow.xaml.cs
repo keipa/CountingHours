2901a74fe6dcb231fdb1bb05457b19b88d81a287
Nickolai Rovdo <nichogaus@gmail.com>
2017-01-09 13:57:56 +0300

    fixed ESC press issue global var wasn't cleared in addition error window added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,17 +25,24 @@ namespace CountingHours
 
         private void AddTime()
         {
-            var one = new DateTime(1, 1, 1, Convert.ToInt32(hours_start.Text), Convert.ToInt32(minutes_start.Text), 0);
-            var two = new DateTime(1, 1, 1, Convert.ToInt32(hours_end.Text), Convert.ToInt32(minutes_end.Text), 0);
-            var addedTime = new DateTime();
-            Time += two - one;
-            addedTime += two - one;
-            if (time.Text != DefaultZeroTime)
-                new_time.Text ="+ " + addedTime.Hour + "h " + addedTime.Minute + "m";
-            time.Text = Time.Hour + "h " + Time.Minute + "m";
-            Clipboard.SetText(time.Text);
-            hours_start.Focus();
-            SelectAllTime();
+            try
+            {
+                var one = new DateTime(1, 1, 1, Convert.ToInt32(hours_start.Text), Convert.ToInt32(minutes_start.Text), 0);
+                var two = new DateTime(1, 1, 1, Convert.ToInt32(hours_end.Text), Convert.ToInt32(minutes_end.Text), 0);
+                var addedTime = new DateTime();
+                Time += two - one;
+                addedTime += two - one;
+                if (time.Text != DefaultZeroTime)
+                    new_time.Text = "+ " + addedTime.Hour + "h " + addedTime.Minute + "m";
+                time.Text = Time.Hour + "h " + Time.Minute + "m";
+                Clipboard.SetText(time.Text);
+                hours_start.Focus();
+                SelectAllTime();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("3n73r NUM83R5", "Shit");
+            }
         }
 
         private void SelectAllTime()
@@ -55,6 +62,7 @@ namespace CountingHours
             minutes_end.Clear();
             time.Text = DefaultZeroTime;
             new_time.Text = "";
+            Time = new DateTime();
         }
 
         private void clear_button_Click(object sender, RoutedEventArgs e) => Reset();
